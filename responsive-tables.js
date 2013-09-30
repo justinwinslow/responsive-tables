560759a6fba8924e7d29d1909f8695fcfb8a5087
Justin Winslow <justin@jwin.me>
2013-09-30 01:31:56 -0500

    Adding hover and updated example

--- responsive-tables.js
+++ responsive-tables.js
@@ -1,6 +1,10 @@
 (function ( $ ) {
 
   $.fn.responsiveTable = function( options ) {
+    options = $.extend({
+      hover: true
+    }, options);
+
     this.each(function(index, el){
       var $el = $(el),
         $wrapper = $el.wrap('<div class="responsiveTableContainer" />').parent(),
@@ -32,6 +36,30 @@
 
       // Add copied table to the wrapper to show it
       $wrapper.append($copy);
+
+      // Handle hover styling
+      if (options.hover) {
+        var hilight = function(event){
+          // get index of hovered tr
+          var index = event.currentTarget.sectionRowIndex;
+
+          // Add hover class to both tables
+          $el.find('tbody > tr:eq(' + index + ')').addClass('hover');
+          $copy.find('tbody > tr:eq(' + index + ')').addClass('hover');
+        },
+        unhilight = function(event){
+          // get index of hovered tr
+          var index = event.currentTarget.sectionRowIndex;
+
+          // Remove hover class
+          $el.find('tbody > tr:eq(' + index + ')').removeClass('hover');
+          $copy.find('tbody > tr:eq(' + index + ')').removeClass('hover');
+        };
+
+        // Listen for hover event and do hilighting
+        $el.find('tr').hover(hilight, unhilight);
+        $copy.find('tr').hover(hilight, unhilight);
+      }
     });
   };
 
