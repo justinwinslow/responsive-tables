f25329f385c67bb2497f502672855e00d4aa3128
Justin Winslow <justin@jwin.me>
2013-09-30 00:31:43 -0500

    Changed block to inline block

--- responsive-tables.js
+++ responsive-tables.js
@@ -13,7 +13,7 @@
 
       // Make the original table scroll
       $el.css({
-        display: 'block',
+        display: 'inline-block',
         'max-width': '100%',
         'overflow-x': 'auto'
       });
